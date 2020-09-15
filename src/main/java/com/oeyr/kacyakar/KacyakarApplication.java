3fe6847e0ca1b6a754a842d9eb34613b28453a25
Özer <ozer.biber@solidict.com>
2020-09-15 23:32:40 +0300

    test

---
+++ src/main/java/com/oeyr/kacyakar/KacyakarApplication.java
@@ -0,0 +1,13 @@
+package com.oeyr.kacyakar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KacyakarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KacyakarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oeyr/kacyakar/KacyakarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oeyr.kacyakar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KacyakarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
