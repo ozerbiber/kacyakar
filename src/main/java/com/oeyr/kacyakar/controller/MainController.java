64167958f86b65a6035969aa2d147ee62b424c28
Özer <ozer.biber@solidict.com>
2020-09-15 23:53:44 +0300

    test

--- src/main/java/com/oeyr/kacyakar/controller/MainController.java
+++ src/main/java/com/oeyr/kacyakar/controller/MainController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/")
 public class MainController {
     @GetMapping
-    public void test(){
-
+    public List<String> test(){
+        return Arrays.asList("a","b","c");
     }
 }
